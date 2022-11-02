23873c3380860a23e35f12ecc656b730525f55a4
donghune <reona7140@naver.com>
2022-11-02 17:12:48 +0900

    kommand 작동!

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.6.21"
 }
 
 
--- terraria-plugins/terraria-auction/src/main/kotlin/io/github/donghune/AuctonCommand.kt
+++ terraria-plugins/terraria-auction/src/main/kotlin/io/github/donghune/AuctonCommand.kt
@@ -3,49 +3,90 @@ package io.github.donghune
 import io.github.donghune.inventory.AuctionCategory
 import io.github.donghune.model.AuctionProduct
 import io.github.donghune.model.create
+import io.github.monun.kommand.KommandArgument.Companion.dynamicByEnum
+import io.github.monun.kommand.getValue
 import io.github.monun.kommand.kommand
-import org.bukkit.command.Command
-import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import java.time.LocalDateTime
 import java.util.*
 
 object AuctonCommand {
+
+    private val auctionCategoryEnum = dynamicByEnum(EnumSet.allOf(AuctionCategory::class.java))
+
+    private val korCommandDescription = listOf(
+        "경매장 등록 <category> <price>"
+    )
+
+    private val engCommandDescription = listOf(
+        "auction register <category> <price>"
+    )
+
     fun initialize(plugin: AuctionPlugin) {
-        val auctionExcutor = object : CommandExecutor {
-            override fun onCommand(
-                sender: CommandSender,
-                command: Command,
-                label: String,
-                args: Array<out String>,
-            ): Boolean {
-                val input = args.joinToString(" ")
-                if (input.matches("등록 [ㄱ-힣]+ \\d+".toRegex()) || input.matches("register [ㄱ-힣]+ \\d+".toRegex())) {
-                    val player = sender as Player
+        plugin.kommand {
+            register("auction") {
+                then("register") {
+                    then("category" to auctionCategoryEnum) {
+                        then("price" to int()) {
+                            executes {
+                                val category: AuctionCategory by it
+                                val price: Int by it
 
-                    val category = AuctionCategory.values().find { it.kor == args[1] }
-                    val price = args[2].toInt()
-
-                    if (category == null) {
-                        player.sendMessage("카테고리가 올바르지 않습니다. 무기/도구/장비/장신구/기타")
-                        return false
+                                registerProduct(
+                                    player,
+                                    category,
+                                    price
+                                )
+                            }
+                        }
                     }
-
-                    AuctionProduct(
-                        uuid = UUID.randomUUID().toString(),
-                        itemStack = player.inventory.itemInMainHand,
-                        seller = player.uniqueId,
-                        price = price,
-                        category = category,
-                        createDate = LocalDateTime.now()
-                    ).create()
-                    player.sendMessage("경매장에 물건이 등록되었습니다.")
                 }
-                return true
+                executes {
+                    sender.sendCommandDescription(engCommandDescription)
+                }
+            }
+            register("경매장") {
+                then("등록") {
+                    then("category" to auctionCategoryEnum) {
+                        then("price" to int()) {
+                            executes {
+                                val category: AuctionCategory by it
+                                val price: Int by it
+
+                                registerProduct(
+                                    player,
+                                    category,
+                                    price
+                                )
+                            }
+                        }
+                    }
+                }
+                executes {
+                    sender.sendCommandDescription(korCommandDescription)
+                }
             }
         }
-        plugin.getCommand("경매장")?.setExecutor(auctionExcutor)
-        plugin.getCommand("auction")?.setExecutor(auctionExcutor)
+    }
+
+    private fun registerProduct(player: Player, category: AuctionCategory, price: Int) {
+        AuctionProduct(
+            uuid = UUID.randomUUID().toString(),
+            itemStack = player.inventory.itemInMainHand,
+            seller = player.uniqueId,
+            price = price,
+            category = category,
+            createDate = LocalDateTime.now()
+        ).create()
+        player.sendMessage("경매장에 물건이 등록되었습니다.")
+    }
+
+    private fun CommandSender.sendCommandDescription(commands : List<String>) {
+        sendMessage("")
+        commands.forEach {
+            sendMessage(it)
+        }
+        sendMessage("")
     }
 }
